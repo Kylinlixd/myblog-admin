e2860298d430bb6450bb5e4ac1c3e685e80a4de6
LiXiaoDong <64355772+Kylinlixd@users.noreply.github.com>
2026-09-13 00:02:14 +0800

    fix: clear comment marker when opening metric

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -21,7 +21,7 @@
       <template v-if="loading">
         <a-skeleton v-for="item in 4" :key="item" active :paragraph="false" class="metric-skeleton" />
       </template>
-      <router-link v-else v-for="item in metrics" :key="item.key" :to="item.path" class="metric-item">
+      <router-link v-else v-for="item in metrics" :key="item.key" :to="item.path" class="metric-item" @click="item.key === 'comments' && acknowledgeCommentCount()">
         <component :is="item.icon" class="metric-icon" />
         <span class="metric-label">{{ item.label }}</span>
         <span class="metric-value-wrap">
@@ -224,6 +224,12 @@ function syncCommentCount(total) {
   } catch {
     commentCountChanged.value = false
   }
+}
+
+function acknowledgeCommentCount() {
+  const current = Number(dashboardData.value.total.comments) || 0
+  try { localStorage.setItem(COMMENT_COUNT_SEEN_KEY, String(current)) } catch { /* storage unavailable */ }
+  commentCountChanged.value = false
 }
 
 function taxonomyShare(item, items) {
